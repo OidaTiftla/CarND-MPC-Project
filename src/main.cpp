fb973826a6d033203faf2a9f9b1179d9606969a2
Christoph Stephan <chm.stephan@outlook.com>
2018-09-30 15:33:25 +0200

    refactor: extract into poly.hpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "MPC.h"
+#include "poly.hpp"
 
 // for convenience
 using json = nlohmann::json;
@@ -30,48 +31,6 @@ string hasData(string s) {
     return s.substr(b1, b2 - b1 + 2);
   }
   return "";
-}
-
-uint factorial(uint n) {
-  uint result = 1;
-  for (uint i = 1; i <= n; ++i) {
-    result *= i;
-  }
-  return result;
-}
-
-// Evaluate a polynomial.
-// If derivative > 0 then the corresponding derivative is evaluated at point x.
-double polyeval(Eigen::VectorXd coeffs, double x, uint derivative = 0) {
-  double result = 0.0;
-  for (int i = derivative; i < coeffs.size(); i++) {
-    result += factorial(i) / factorial(i - derivative) * coeffs[i] * pow(x, i - derivative);
-  }
-  return result;
-}
-
-// Fit a polynomial.
-// Adapted from
-// https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
-Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
-                        int order) {
-  assert(xvals.size() == yvals.size());
-  assert(order >= 1 && order <= xvals.size() - 1);
-  Eigen::MatrixXd A(xvals.size(), order + 1);
-
-  for (int i = 0; i < xvals.size(); i++) {
-    A(i, 0) = 1.0;
-  }
-
-  for (int j = 0; j < xvals.size(); j++) {
-    for (int i = 0; i < order; i++) {
-      A(j, i + 1) = A(j, i) * xvals(j);
-    }
-  }
-
-  auto Q = A.householderQr();
-  auto result = Q.solve(yvals);
-  return result;
 }
 
 // convert map coordinates (map_x, map_y) into car coordinates (car_x, car_y)
