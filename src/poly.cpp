841b13a3a277a918ff6a0a3b23822337eb785116
Christoph Stephan <chm.stephan@outlook.com>
2018-09-30 18:32:38 +0200

    CppAD::pow cannot handle variables in the second argument

--- src/poly.cpp
+++ src/poly.cpp
@@ -20,6 +20,77 @@ double polyeval(const Eigen::VectorXd &coeffs, double x, const uint derivative /
   return result;
 }
 
+CppAD::AD<double> polyHelperPowAD(const CppAD::AD<double> &x, const uint pow) {
+  // CppAD::pow cannot handle variables in the second argument
+  // here the function is unrolled
+  //
+  // This is the error, if it is not unrolled:
+  // cppad-20180000.0 error from a known source:
+  // yq = f.Forward(q, xq): has a non-zero order Taylor coefficient
+  // with the value nan (but zero order coefficients are not nan).
+  // Error detected by false result for
+  //     ok
+  // at line 251 in the file
+  //     /usr/include/cppad/core/forward.hpp
+  // mpc: /usr/include/cppad/utility/error_handler.hpp:206: static void CppAD::ErrorHandler::Default(bool, int, const char*, const char*, const char*): Assertion `false' failed.
+  // ./run.sh: line 9:  4436 Aborted                 (core dumped) ./mpc
+  if (0 == pow) {
+    return CppAD::pow(x, 0);
+  }
+  if (1 == pow) {
+    return CppAD::pow(x, 1);
+  }
+  if (2 == pow) {
+    return CppAD::pow(x, 2);
+  }
+  if (3 == pow) {
+    return CppAD::pow(x, 3);
+  }
+  if (4 == pow) {
+    return CppAD::pow(x, 4);
+  }
+  if (5 == pow) {
+    return CppAD::pow(x, 5);
+  }
+  if (6 == pow) {
+    return CppAD::pow(x, 6);
+  }
+  if (7 == pow) {
+    return CppAD::pow(x, 7);
+  }
+  if (8 == pow) {
+    return CppAD::pow(x, 8);
+  }
+  if (9 == pow) {
+    return CppAD::pow(x, 9);
+  }
+  if (10 == pow) {
+    return CppAD::pow(x, 10);
+  }
+  if (11 == pow) {
+    return CppAD::pow(x, 11);
+  }
+  if (12 == pow) {
+    return CppAD::pow(x, 12);
+  }
+  if (13 == pow) {
+    return CppAD::pow(x, 13);
+  }
+  if (14 == pow) {
+    return CppAD::pow(x, 14);
+  }
+  if (15 == pow) {
+    return CppAD::pow(x, 15);
+  }
+  if (16 == pow) {
+    return CppAD::pow(x, 16);
+  }
+  if (17 == pow) {
+    std::cout << "Error (polyHelperPowAD): for loop must be unrolled for more iterations!!!" << std::endl;
+  }
+  return 0.0;
+}
+
 CppAD::AD<double> polyevalAD(const Eigen::VectorXd &coeffs, const CppAD::AD<double> &x, const uint derivative /*= 0*/) {
   CppAD::AD<double> result = 0.0;
   for (uint i = derivative; i < coeffs.size(); i++) {
@@ -27,7 +98,7 @@ CppAD::AD<double> polyevalAD(const Eigen::VectorXd &coeffs, const CppAD::AD<doub
     if (derivative > 0) {
       factor = factorial(i) / factorial(i - derivative);
     }
-    result += factor * coeffs[i] * CppAD::pow(x, i - derivative);
+    result += factor * coeffs[i] * polyHelperPowAD(x, i - derivative);
   }
   return result;
 }
