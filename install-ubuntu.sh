848c40169e1bb46ce9a865cab4b13ecdd24fd1c2
Christoph Stephan <chm.stephan@outlook.com>
2018-10-02 14:37:05 +0200

    update install script

--- install-ubuntu.sh
+++ install-ubuntu.sh
@@ -1,5 +1,29 @@
 #! /bin/bash
+
+# update
 sudo apt-get update
+
+# gfortran dependency
+sudo apt-get install gfortran
+
+# get unzip
+sudo apt-get install unzip
+
+# Ipopt: get, install, unzip
+wget https://www.coin-or.org/download/source/Ipopt/Ipopt-3.12.11.zip && unzip Ipopt-3.12.11.zip && rm Ipopt-3.12.11.zip
+./install_ipopt.sh ./Ipopt-3.12.11
+
+# CppAD
+sudo apt-get install cppad
+
+# Gnuplot
+sudo apt-get install gnuplot
+
+# python and matplotlib
+sudo apt-get install python-matplotlib
+sudo apt-get install python2.7-dev
+
+# uWebSockets dependency
 sudo apt-get install git libuv1-dev libssl-dev gcc g++ cmake make
 git clone https://github.com/uWebSockets/uWebSockets 
 cd uWebSockets
